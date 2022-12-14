b24f49fa87590c7e924c1afa8add02c6352f4df7
umeshuy <110802360+umeshuy@users.noreply.github.com>
2022-12-14 12:04:28 +0530

    adding delete btn

--- dom.js
+++ dom.js
@@ -1,25 +1,76 @@
 // Traversing the DOM
-var = itemList = document.querySelector('#items');
-console.log(itemList.parentNode);
-console.log(itemList.children);
-console.log(itemList.children[1]);
-itemList.children[1].style.backgroundColor = 'yellow';
+// var = itemList = document.querySelector('#items');
+// console.log(itemList.parentNode);
+// console.log(itemList.children);
+// console.log(itemList.children[1]);
+// itemList.children[1].style.backgroundColor = 'yellow';
 
-console.log(itemList.firstElementChild);
-itemList.lastElementChild.innerHTML = 'hello';
+// console.log(itemList.firstElementChild);
+// itemList.lastElementChild.innerHTML = 'hello';
 
-console.log(itemList.nextElementSibling);
+// console.log(itemList.nextElementSibling);
 
 // create a div
-var newDiv = document.createElement('div');
-newDiv.className = 'hello';
-newDiv.id = 'bye';
+// var newDiv = document.createElement('div');
+// newDiv.className = 'hello';
+// newDiv.id = 'bye';
 
-newDiv.setAttribute('title','hello div');
-var divText = document.createTextNode('heelo my frnds');
+// newDiv.setAttribute('title','hello div');
+// var divText = document.createTextNode('heelo my frnds');
 
-newDiv.appendChild(divText);
-var conatiner = document.querySelector('header .contanier');
-var h1 = document.querySelector('header h1');
-console.log(newDiv);
- conatiner.insertBefore(newDiv,h1);
+// newDiv.appendChild(divText);
+// var conatiner = document.querySelector('header .contanier');
+// var h1 = document.querySelector('header h1');
+// console.log(newDiv);
+//  conatiner.insertBefore(newDiv,h1);
+var form = document.getElementById('addForm');
+var itemList = document.getElementById('items');
+var filter = document.getElementById('filter');
+
+// Form submit event
+form.addEventListener('submit', addItem);
+// Delete event
+itemList.addEventListener('click', removeItem);
+// Filter event
+filter.addEventListener('keyup', filterItems);
+
+// Add item
+function addItem(e){
+  e.preventDefault();
+
+  // Get input value
+  var newItem = document.getElementById('item').value;
+
+  // Create new li element
+  var li = document.createElement('li');
+  // Add class
+  li.className = 'list-group-item';
+  // Add text node with input value
+  li.appendChild(document.createTextNode(newItem));
+
+  // Create del button element
+  var deleteBtn = document.createElement('button');
+
+  // Add classes to del button
+  deleteBtn.className = 'btn btn-danger btn-sm float-right delete';
+
+  // Append text node
+  deleteBtn.appendChild(document.createTextNode('X'));
+
+  // Append button to li
+  li.appendChild(deleteBtn);
+
+  // Append li to list
+  itemList.appendChild(li);
+}
+
+// Remove item
+function removeItem(e){
+  if(e.target.classList.contains('delete')){
+    if(confirm('Are You Sure?')){
+      var li = e.target.parentElement;
+      itemList.removeChild(li);
+    }
+  }
+}
+
